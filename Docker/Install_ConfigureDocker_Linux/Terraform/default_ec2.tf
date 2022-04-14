a2be32e9c4592174726896bcd73a3b974884c003
Engin <engingltekin@gmail.com>
2022-04-13 21:50:30 -0400

    Install Docker on Linux  with AWS CFN or Terraform

---
+++ Docker/Install_ConfigureDocker_Linux/Terraform/default_ec2.tf
@@ -0,0 +1,61 @@
+data "aws_vpc" "main_vpc" {
+  id = "vpc-065740f91024a5ae2"
+}
+
+data "aws_ami" "tf_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_security_group" "ec2-instance-SG" {
+  name        = "Ec2-SG"
+  description = "Allow SSH and HTTP"
+  vpc_id      = data.aws_vpc.main_vpc.id
+  // To Allow SSH Transport
+  ingress {
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "default_ec2" {
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.tf_ami.id
+  key_name      = "Engin_Linux"
+  user_data     = file("./post_configuration.sh")
+  vpc_security_group_ids = [
+    aws_security_group.ec2-instance-SG.id
+  ]
+  tags = {
+    "Name" = "Docker instance"
+  }
+}
+
+output "publicip" {
+  value = aws_instance.default_ec2.public_ip
+}
---
+++ Docker/Install_ConfigureDocker_Linux/Terraform/post_configuration.sh
@@ -0,0 +1,11 @@
+yum update -y
+amazon-linux-extras install docker -y
+systemctl start docker
+systemctl enable docker
+systemctl status docker
+usermod -a -G docker ec2-user # add user to docker group
+newgrp docker
+docker version
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" \
+-o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
