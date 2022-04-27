ae3120d3ef6b1989173cd974393284c9f802ec93
Engin <engingltekin@gmail.com>
2022-04-26 21:39:04 -0400

    Phonebook containerization

---
+++ Docker/303-dockerization-phonebook-api/Terraform/data.tf
@@ -0,0 +1,34 @@
+data "aws_vpc" "main_vpc" {
+  default = true
+}
+
+data "aws_ami" "tf_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+# data "aws_ami" "ubuntu" {
+#   most_recent = true
+
+#   filter {
+#     name   = "name"
+#     values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+#   }
+
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+
+#   owners = ["amazon"] # Canonical
+# }
